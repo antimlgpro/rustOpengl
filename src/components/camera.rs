a15b90a6c5fc0916f594e728f6639b90df484608
Ludvig <ludvigvonreis@gmail.com>
2022-01-28 11:37:02 +0100

    Reworked camera functions

--- src/components/camera.rs
+++ src/components/camera.rs
@@ -7,7 +7,8 @@ pub struct Camera {
 	pub fov: f32,
 	pub aspect_ratio: f32,
 
-	pub projection_view: Matrix4<f32>,
+	pub projection: Matrix4<f32>,
+	pub view: Matrix4<f32>,
 }
 
 impl Default for Camera {
@@ -16,25 +17,31 @@ impl Default for Camera {
 			mouse_sensitivity: 0.1,
 			fov: 90.0,
 			aspect_ratio: 1.0,
-			projection_view: Matrix4::default(),
+			projection: Matrix4::default(),
+			view: Matrix4::default(),
 		}
 	}
 }
 
 impl Camera {
-	pub fn get_view_matrix(&self, tf: &Transform) -> Matrix4<f32> {
-		let pos: &Point3<f32> = &tf.position.into();
-		let sm: Point3<f32> = (tf.position + tf.forward).into();
-		//let sm = Point3::new(0.0, 0.0, 0.0);
-		return Matrix4::look_at_rh(pos, &sm, &tf.up);
-	}
-
-	pub fn update_projection_view(&mut self, transform: &Transform) {
+	pub fn update_projection(&mut self) {
 		let vertical = 2.0 * ((self.fov / 2.0).tan() * self.aspect_ratio).atan();
 		let projection: Perspective3<f32> =
 			Perspective3::new(self.aspect_ratio, vertical, 0.1, 100.0);
 
-		let view = self.get_view_matrix(transform);
-		self.projection_view = projection.as_matrix() * view;
+		self.projection = projection.to_homogeneous();
+	}
+
+	pub fn update_view(&mut self, tf: &Transform) {
+		let pos: &Point3<f32> = &tf.position.into();
+		let sm: Point3<f32> = (tf.position + tf.forward).into();
+		//println!("x:{} y:{} z:{}", tf.forward.x, tf.forward.y, tf.forward.z);
+
+		self.view = Matrix4::look_at_rh(pos, &sm, &tf.up);
+	}
+
+	pub fn update(&mut self, tf: &Transform) {
+		self.update_projection();
+		self.update_view(tf);
 	}
 }

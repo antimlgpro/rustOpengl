6608ae24c2993d5077bc9eda3ef743e142cad101
Ludvig <ludvigvonreis@gmail.com>
2022-01-28 11:31:43 +0100

    Removed temporary lighting stuff

--- shaders/advanced.fs
+++ shaders/advanced.fs
@@ -1,10 +1,7 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 object_color;
-uniform vec3 light_color;
-
 void main()
 {
-    FragColor = vec4(light_color * object_color, 1.0);
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }

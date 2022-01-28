f78ceb3478d9a26e5ee46e37189b49e0cc1886c8
Ludvig <ludvigvonreis@gmail.com>
2022-01-28 17:23:42 +0100

    Made camera create projection matrix when created

--- src/components/camera.rs
+++ src/components/camera.rs
@@ -13,13 +13,16 @@ pub struct Camera {
 
 impl Default for Camera {
 	fn default() -> Camera {
-		Camera {
+		let mut cam = Camera {
 			mouse_sensitivity: 0.1,
 			fov: 90.0,
 			aspect_ratio: 1.0,
 			projection: Matrix4::default(),
 			view: Matrix4::default(),
-		}
+		};
+		cam.update_projection();
+
+		cam
 	}
 }
 
@@ -35,13 +38,7 @@ impl Camera {
 	pub fn update_view(&mut self, tf: &Transform) {
 		let pos: &Point3<f32> = &tf.position.into();
 		let sm: Point3<f32> = (tf.position + tf.forward).into();
-		//println!("x:{} y:{} z:{}", tf.forward.x, tf.forward.y, tf.forward.z);
 
 		self.view = Matrix4::look_at_rh(pos, &sm, &tf.up);
-	}
-
-	pub fn update(&mut self, tf: &Transform) {
-		self.update_projection();
-		self.update_view(tf);
 	}
 }

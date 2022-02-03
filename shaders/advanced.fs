3f0bd04669a7f803815a907346869be6ae5a7247
Ludvig <ludvigvonreis@gmail.com>
2022-02-03 16:57:12 +0100

    Changed object color into albedo

--- shaders/advanced.fs
+++ shaders/advanced.fs
@@ -1,9 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 object_color;
+uniform vec3 albedo;
 
 void main()
 {
-    FragColor = vec4(object_color, 1.0);
+    FragColor = vec4(albedo, 1.0);
 }

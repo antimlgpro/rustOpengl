1824367e0b492cced69076f7d879c8ce3af2da7c
Ludvig <ludvigvonreis@gmail.com>
2022-02-03 14:38:43 +0100

    Added normal genration for models that did not come with them

--- src/wrapper/rendering/model_loader.rs
+++ src/wrapper/rendering/model_loader.rs
@@ -1,4 +1,7 @@
-use crate::wrapper::{GLError, Mesh, Vertex};
+use crate::{
+	util::calculate_normals,
+	wrapper::{GLError, Mesh, Vertex},
+};
 use nalgebra::{vector, Vector3};
 use std::path::Path;
 use tobj;
@@ -27,7 +30,30 @@ impl Loader {
 			let indices: Vec<u32> = mesh.indices.clone();
 
 			let (p, n) = (&mesh.positions, &mesh.normals);
+
+			let mut positions: Vec<Vector3<f32>> = Vec::new();
 			for i in (0..num_vertices).step_by(3) {
+				positions.push(vector!(p[i], p[i + 1], p[i + 2]));
+			}
+
+			let mut normals: Vec<Vector3<f32>> = Vec::new();
+			if n.len() != num_vertices {
+				normals = calculate_normals(&positions, &indices);
+			} else {
+				for i in (0..num_vertices).step_by(3) {
+					normals.push(vector!(n[i], n[i + 1], n[i + 2]));
+				}
+			}
+
+			for i in 0..positions.len() {
+				vertices.push(Vertex {
+					position: positions[i],
+					normal: normals[i],
+					..Vertex::default()
+				});
+			}
+
+			/* for i in (0..num_vertices).step_by(3) {
 				let mut position = Vector3::<f32>::default();
 				let mut normal = Vector3::<f32>::default();
 
@@ -44,7 +70,7 @@ impl Loader {
 					normal,
 					..Vertex::default()
 				});
-			}
+			} */
 
 			let mesh = match Mesh::new(vertices, indices) {
 				Ok(e) => e,

8cc9e64a9bbf6e80eae700bac0afd99fc8fa5350
Ludvig <ludvigvonreis@gmail.com>
2022-02-03 17:07:58 +0100

    Removed todo

--- src/wrapper/rendering/shader.rs
+++ src/wrapper/rendering/shader.rs
@@ -37,17 +37,12 @@ impl Clone for Shader {
 }
 
 impl Shader {
-	/// Opens and creates a shader program
-	///
-	/// * `v_src_path` - String path to vertex shader
-	/// * `f_src_path` - String path to fragment shader
 	pub fn new(v_src_path: &str, f_src_path: &str) -> Result<Shader, ShaderError> {
 		let mut shader = Shader { id: 0 };
 
 		let vertex_src = to_cstring(load_to_string(v_src_path).unwrap()).unwrap();
 		let fragment_src = to_cstring(load_to_string(f_src_path).unwrap()).unwrap();
 
-		// TODO check shader compile errors
 		unsafe {
 			let vertex = gl::CreateShader(gl::VERTEX_SHADER);
 			gl::ShaderSource(vertex, 1, &vertex_src.as_ptr(), ptr::null());
